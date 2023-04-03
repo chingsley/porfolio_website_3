990882459062768543d37149ed7470b312357236
Kingsley Eneja <kingsley.eneja@getir.com>
2023-04-03 19:51:56 +0100

    Setup the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+import './css/App.css';
+import { AppHeader, ScrollToTop } from './components';
+import UseScrollTop from './hooks/useScrollToTop';
+
+const AboutMe = lazy(() => import('./pages/AboutMe'));
+const Contact = lazy(() => import('./pages/Contact'));
+const Home = lazy(() => import('./pages/Home'));
+const Projects = lazy(() => import('./pages/Projects'));
+const ProjectSingle = lazy(() => import('./pages/ProjectSingle'));
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <AnimatePresence>
+      <div className="bg-secondary-light dark:bg-primary-dark transition duration-300">
+        <Router>
+          <ScrollToTop />
+          <AppHeader />
+          <Suspense fallback={""}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="projects" element={<Projects />} />
+              <Route path="projects/single-project" element={<ProjectSingle />} />
+              <Route path="about" element={<AboutMe />} />
+              <Route path="contact" element={<Contact />} />
+            </Routes>
+          </Suspense>
+        </Router>
+        <UseScrollTop />
+      </div>
+    </AnimatePresence>
   );
 }
 
