93ab484b71cc9a40f51426efa515b6593c8dd96c
Kingsley Eneja <kingsley.eneja@getir.com>
2023-04-04 18:29:30 +0100

    Implement the useThemeSwitcher hook

--- src/hooks/useThemeSwitcher.tsx
+++ src/hooks/useThemeSwitcher.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
 const useThemeSwitcher = () => {
-  return (
-    <div>useThemeSwitcher</div>
-  );
+  const [theme, setTheme] = useState<"dark" | "light">(localStorage.theme);
+  const activeTheme = theme === 'dark' ? 'light' : 'dark';
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+
+    root.classList.remove(activeTheme);
+    root.classList.add(theme);
+    localStorage.setItem('theme', theme);
+  }, [theme, activeTheme]);
+
+  return [activeTheme, setTheme] as const;
 };
 
 export default useThemeSwitcher;
