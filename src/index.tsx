6da79cf67dae593f0e12637c2eb94c9c24278794
Kingsley Eneja <kingsley.eneja@getir.com>
2023-04-10 08:13:36 +0100

    Implement switch between grid and list views

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ProjectsProvider from './context/ProjectsContext';
+import SettingsProvider from './context/SettingsContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ProjectsProvider>
-      <App />
+      <SettingsProvider>
+        <App />
+      </SettingsProvider>
     </ProjectsProvider>
   </React.StrictMode>
 );
