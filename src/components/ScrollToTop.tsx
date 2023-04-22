80eee61c59ef96bfd51811368e9c6e4b9bcd0f57
Kingsley Eneja <kingsley.eneja@getir.com>
2023-04-22 23:30:30 +0100

    Implement component: ScrollToTop

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+// NOTE: This scroll to top is the default react scroll to top behavior when visiting a new route.
+// For the scroll to top behavior on a click event I will creat a custom hook with the name of useScrollToTop under the hooks folder that scrolls use to the top of the page when they scroll down on the page.
+
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = () => {
-  return (
-    <div>ScrollToTop</div>
-  );
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
 };
 
 export default ScrollToTop;
