54c5e4084d63bf5f1076bad9f0347bf27b71fb21
Kingsley Eneja <kingsley.eneja@getir.com>
2023-04-27 22:43:49 +0100

    Implement component: AboutMeBio

--- src/components/about/AboutMeBio.tsx
+++ src/components/about/AboutMeBio.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import aboutMeText from '../../data/aboutMeText';
+import profileImage from '../../images/profile.jpeg';
 
 const AboutMeBio = () => {
   return (
-    <div>AboutMeBio</div>
+
+    <div className="block sm:flex sm:gap-10 mt-10 sm:mt-20">
+      <div className="w-full sm:w-1/3 mb-7 sm:mb-0">
+        <img src={profileImage} className="rounded-lg w-96" alt="" />
+      </div>
+
+      <div className="font-general-regular w-full sm:w-3/4 text-left">
+        <p
+          className="mb-4 text-ternary-dark dark:text-ternary-light text-lg"
+        >
+          {aboutMeText}
+        </p>
+      </div>
+    </div>
+
   );
 };
 
--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import { aboutMe as aboutMeText } from '../data';
+import { AboutMeBio } from '../components';
+
 
 const AboutMe = () => {
+  const [aboutMe, setAboutMe] = useState(aboutMeText);
   return (
-    <div>AboutMe</div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className="container mx-auto"
+    >
+      <AboutMeBio />
+    </motion.div>
   );
 };
 
