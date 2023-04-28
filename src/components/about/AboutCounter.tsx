bcc6158241aecfc5585c415f2d171cf0435655a0
Kingsley Eneja <kingsley.eneja@getir.com>
2023-04-28 16:12:41 +0100

    Implement component: AboutMeCounter

--- src/components/about/AboutCounter.tsx
+++ src/components/about/AboutCounter.tsx
@@ -1,8 +1,40 @@
-import React from 'react';
+import { useCountUp } from 'react-countup';
+import CounterItem from './CounterItem';
 
 const AboutCounter = () => {
+  useCountUp({ ref: 'experienceCounter', end: 12, duration: 2 });
+  useCountUp({ ref: 'githubStarsCounter', end: 20, duration: 2 });
+  useCountUp({ ref: 'feedbackCounter', end: 92, duration: 2 });
+  useCountUp({ ref: 'projectsCounter', end: 77, duration: 2 });
+
   return (
-    <div>AboutCounter</div>
+    <div className="mt-10 sm:mt-20 bg-primary-light dark:bg-ternary-dark shadow-sm">
+      <div className="font-general-medium container mx-auto py-40 block sm:flex sm:justify-between items-center custom-band">
+        <CounterItem
+          title="Years of experience"
+          counter={<span id="experienceCounter" />}
+          measurement=""
+        />
+
+        <CounterItem
+          title="Stars on GitHub"
+          counter={<span id="githubStarsCounter" />}
+          measurement="k+"
+        />
+
+        <CounterItem
+          title="Positive feedback"
+          counter={<span id="feedbackCounter" />}
+          measurement="%"
+        />
+
+        <CounterItem
+          title="Projects completed"
+          counter={<span id="projectsCounter" />}
+          measurement="%"
+        />
+      </div>
+    </div>
   );
 };
 
--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,20 +1,31 @@
 import { useState } from 'react';
 import { motion } from 'framer-motion';
 import { aboutMe as aboutMeText } from '../data';
-import { AboutMeBio } from '../components';
+import { AboutCounter, AboutMeBio } from '../components';
 
 
 const AboutMe = () => {
   const [aboutMe, setAboutMe] = useState(aboutMeText);
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      className="container mx-auto"
-    >
-      <AboutMeBio />
-    </motion.div>
+    <>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        className="container mx-auto"
+      >
+        <AboutMeBio />
+      </motion.div>
+
+      {/** Counter without paddings */}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
+        <AboutCounter />
+      </motion.div>
+    </>
   );
 };
 
